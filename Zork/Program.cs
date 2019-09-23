5e98a0cd9ef4a43c0adf330f8b764d709bc7c685
brad <brad18@bellsouth.net>
2019-09-23 15:00:40 -0400

    10 - File Processing

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
@@ -17,7 +18,8 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescriptions();
+            string roomsFilename = "Rooms.txt";
+            initializeRoomDescriptions(roomsFilename);
 
             Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
@@ -103,26 +105,47 @@ namespace Zork
             { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
-        private static void InitializeRoomDescriptions()
+        private static readonly Dictionary<string, Room> RoomMap;
+        private static void initializeRoomDescriptions(string roomsFilename)
         {
-            var roomMap = new Dictionary<string, Room>();
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+          
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(fieldDelimiter);
+                if (fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid record.");
+                }
+
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
+
+                RoomMap[name].Description = description;
+            }
+        }
+            
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
+
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
+
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
             foreach (Room room in Rooms)
             {
-                roomMap[room.Name] = room;
+                RoomMap[room.Name] = room;
             }
 
-            roomMap["Rocky Trail"] = Rooms[0, 0];
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail."; 
-            roomMap["South of House"].Description = "You are facing the south side of a white house.  There is no door here, and all the windows are barred."; 
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";  
-
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";  
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door."; 
-            roomMap["Behind House"].Description = "You are behind the white house.  In one corner of the house there is a small window which is slightly ajar.";
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around.  To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house.  There is no door here, and all the windows are barred.";  
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";  
         }
 
         private static readonly List<Commands> Directions = new List<Commands>
